63949c9ab9b7de9222474c96e1e4a4bdd8499480
mili Arsema <meleymm88@gmail.com>
2026-04-08 15:53:36 -0500

    Created variables

--- src/main/java/com/pluralsight/CreatingVariables.java
+++ src/main/java/com/pluralsight/CreatingVariables.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class CreatingVariables {
+public static void main(String[] args) {
+    // declaring variables
+
+      String vin = "00W33y1";
+      String vehicleModel = "Ford";
+      String vehicleColor = "Red";
+      boolean hasTowing = true;
+      int odometer = 0;
+      int price = 0;
+      char rating = 'A';
+      int phoneNumber = 150551511;
+      int ssn = 77766555;
+      int zipCode = 77337;
+}
 }
